b17dc8a0cef4b11581af545bf67cc46a9e932c9a
Ghost10268 <133873716+Ghost10268@users.noreply.github.com>
2023-06-12 15:14:23 +0300

    Add files via upload

---
+++ Text.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_practice
+{
+    class Text
+    {
+        public void Welcome()
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("| Здравствуй Самурай ты попал в мир страха и чтобы отсюда |");
+            Console.WriteLine("| выбраться тебе придется очень постараться и купить      |");
+            Console.WriteLine("| в киоске особый предмет который сможет вытащить тебя    |");
+            Console.WriteLine("| отсюда. Стоимость этого предмета 100 злата,             |");
+            Console.WriteLine("| но заработать его не просто. В процессе поиска          |");
+            Console.WriteLine("| злата для покупки предмета ты встретишься с монстрами   |");
+            Console.WriteLine("| разной сложности, которых без покупки улучшающих        |");
+            Console.WriteLine("| предметов и зелей исцеления тебе не победить.           |");
+            Console.WriteLine("| И запомни, у Самурая нет цели, есть только путь!        |");
+            Console.WriteLine("| В общем и целом, удачи в поисках и желаю не умереть)     |");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+        public void Game_over()
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("|УРААА!!! ПОЗДРАВЛЯЮ!!!!                                 |");
+            Console.WriteLine("|Вы смогли пройти этот путь и познать долг Самурая!      |");
+            Console.WriteLine("| Теперь ты готов к трудностям на своём пути             |");
+            Console.WriteLine("| И когда у тебя будут проблемы                          |");
+            Console.WriteLine("| вспомни как тяжело было Самураю                        |");
+            Console.WriteLine("|                                                        |");
+            Console.WriteLine("|                                                        |");
+            Console.WriteLine("| Поддержи автора и скинь на будущие проекты             |");
+            Console.WriteLine("|   2202 2018 6401 6254       8-987-659-27-17            |");
+            Console.WriteLine("|                 Артём Денисович Ш.                     |");
+            Console.WriteLine("| Поддержи сценариста и скинь ему на обед в школе        |");
+            Console.WriteLine("|   2202 2036 9663 4148       8-988-011-20-28            |");
+            Console.WriteLine("|                 Дмитрий Антонович Г.                   |");
+            Console.WriteLine("| Над этой игрой угарали:                                |");
+            Console.WriteLine("| Автор: Шевченко Артём (Студент)♥                       |");
+            Console.WriteLine("| Сценарист: Горбунов Дмитрий (Малой)♥                   |");
+            Console.WriteLine("| В главных ролях семья из джунглей:                     |");
+            Console.WriteLine("| Бобёр - Максим Кузьминцев (Маугли)                     |");
+            Console.WriteLine("| Зуко - Полина Зуева(Багира)                            |");
+            Console.WriteLine("| Костян - Константин Бобров(Внеплановый сын)            |");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+    }
+}
